2b45b9ecdb5c922c3e2255114118364e52fced86
polykisa <s.sanek2005@gmail.com>
2024-03-18 13:07:18 +0300

    i hope its okay

--- skopchenko.alexander/P3/excludeChars.cpp
+++ skopchenko.alexander/P3/excludeChars.cpp
@@ -10,4 +10,19 @@ void skopchenko::excludeCharacters(char *str1, char *str2)
     asciiMap[(int)*str2] = 1;
     str2++;
   }
+
+  char *result = new char[10000];
+
+  int i = 0;
+  while (*str1 != '\0')
+  {
+    if (asciiMap[(int)*str1] == 0) {
+      result[i] = *str1;
+      i++;
+    }
+    str1++;
+  }
+  result[i] = '\0';
+  std::cout << result << '\n';
+  delete[] result;
 }
