125415df96acf936ef50b5d0173b40fede57c214
polykisa <s.sanek2005@gmail.com>
2024-04-06 14:17:09 +0300

    решил делать по-другому

--- skopchenko.alexander/T1/rectangle.cpp
+++ skopchenko.alexander/T1/rectangle.cpp
@@ -1,11 +1,32 @@
 #include "rectangle.hpp"
 #include <stdexcept>
 
-skopchenko::Rectangle::Rectangle(const point_t &left_down, const point_t &right_up):
-  rectangle _{right_up.x - left_down.x, right_up.y - left_down.y, {(left_down.x + right_up.x)/2, (left_down.y + right_up.y)/2}}
+Rectangle::Rectangle(point_t left_down, point_t right_up):
+  left_down_(left_down),
+  right_up_(right_up)
+{}
+
+rectangle_t Rectangle::getFrameRectangle() const
 {
-  if (left_down.x >= right_up || left_down.y >= right_up.y)
-  {
-    throw std::invalid_argument();
-  }
+  point_t side = {right_up_.x - left_down_.x, right_up_.y - left_down_.y};
+  point_t position = {(left_down_.x + right_up_.x) / 2, (left_down_.y + right_up_.y) / 2};
+  return {side.y, side.x, position};
 }
+
+double Rectangle::getArea() const
+{
+  double area_rectangle = getFrameRectangle().width * getFrameRectangle().height;
+  return area_rectangle;
+}
+
+void Rectangle::scale(double k)
+{
+  if (k <= 0)
+  {
+    throw std::invalid_argument("k under zero");
+  }
+  point_t center = getFrameRectangle().position;
+  left_down_ = baseFunctions::makeScale(left_down_, center, k);
+  right_up_ = baseFunctions::makeScale(right_up_, center, k);
+}
+
--- skopchenko.alexander/T1/rectangle.hpp
+++ skopchenko.alexander/T1/rectangle.hpp
@@ -1,22 +1,20 @@
 #ifndef RECTANGLE_HPP
 #define RECTANGLE_HPP
 
-#include <shape.hpp>
+#include "shape.hpp"
 
-namespace skopchenko
+class Rectangle: public Shape
 {
-  class Rectangle: public Shape
-  {
-   public:
-    Rectangle(const point_t & left_down, const point_t & right_up);
-    double getArea() const override;
-    rectangle_t getFrameRect() const override;
-    void move(const point_t & position) override;
-    void move(double dx, double dy) override;
-    void scale(double k) override;
-  private:
-    rectangle_t rectangle_;
-  };
-}
+public:
+  Rectangle(const point_t & left_down, const point_t & right_up);
+  double getArea() const override;
+  rectangle_t getFrameRect() const override;
+  void move(const point_t & position) override;
+  void move(double dx, double dy) override;
+  void scale(double k) override;
+private:
+  point_t left_down_;
+  point_t right_up
+};
 
 #endif
