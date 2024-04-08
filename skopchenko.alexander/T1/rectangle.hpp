f9c57b898aa2a4c4453581820460d41f1eb388f4
polykisa <s.sanek2005@gmail.com>
2024-04-08 17:14:26 +0300

    хз че это такое и как оно будет работать, но если свечку поставить - может сработает

--- skopchenko.alexander/T1/rectangle.hpp
+++ skopchenko.alexander/T1/rectangle.hpp
@@ -8,7 +8,7 @@ class Rectangle: public Shape
 public:
   Rectangle(point_t left_down, point_t right_up);
   double getArea() const override;
-  rectangle_t getFrameRectangle() const;
+  rectangle_t getFrameRectangle() const override;
   void move(point_t position) override;
   void move(double dx, double dy) override;
   void scale(double k) override;
--- skopchenko.alexander/T1/scaling.cpp
+++ skopchenko.alexander/T1/scaling.cpp
@@ -7,7 +7,7 @@ void scale(Shape* shape, point_t point, double k)
   {
     throw std::invalid_argument("k shouldn't be under 0");
   }
-  point_t pos{ shape->getFrameRect().pos.x, shape->getFrameRect().pos.y};
+  point_t pos{ shape->getFrameRectangle().pos.x, shape->getFrameRectangle().pos.y};
   double dx = point.x - pos.x;
   double dy = point.y - pos.y;
   shape->move(dx, dy);
